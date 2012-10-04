d6ad786d01a1cd4b0ba2cc07aa088c89c1eaa244
Zoltán Nagy <abesto@abesto.net>
2012-10-04 21:34:50 +0200

    PASS

--- katas/prime-factors/ruby-20121004-1/spec.rb
+++ katas/prime-factors/ruby-20121004-1/spec.rb
@@ -1,16 +1,14 @@
 require './code'
 
 describe 'prime_factors' do
-  it 'returns [] for 0' do
-    prime_factors(0).should eq([])
-  end
-
-  it 'returns [] for 1' do
-    prime_factors(1).should eq([])
-  end
-
-  it 'returns [2] for 2' do
-    prime_factors(2).should eq([2])
+  {
+      0 => [],
+      1 => [],
+      2 => [2]
+  }.each do |input, output|
+    it "returns #{output} for #{input}" do
+      prime_factors(input).should eq(output)
+    end
   end
 end
 
