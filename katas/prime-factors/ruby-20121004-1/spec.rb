ba6d6460d379ff42e355c376c9e7ed262c8058e2
Zoltán Nagy <abesto@abesto.net>
2012-10-04 21:37:37 +0200

    PASS

--- katas/prime-factors/ruby-20121004-1/spec.rb
+++ katas/prime-factors/ruby-20121004-1/spec.rb
@@ -6,7 +6,8 @@ describe 'prime_factors' do
       1 => [],
       2 => [2],
       3 => [3],
-      4 => [2,2]
+      4 => [2,2],
+      5 => [5]
   }.each do |input, output|
     it "returns #{output} for #{input}" do
       prime_factors(input).should eq(output)
